9fbc0175e375b2a9b7852f079bd87d69a51f6166
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-09-04 14:03:40 -0300

    removing lines startig with [

--- webserver_log_analysis/pre-process-log.sh
+++ webserver_log_analysis/pre-process-log.sh
@@ -9,8 +9,8 @@ then
     exit 1
 fi
 
-
-awk '{print $1,$5,$6,$11,$13}' $FILENAME > ${FILENAME}.preprocessed
+awk '{print $7,$11,$12,$17,$19}' $FILENAME > ${FILENAME}.preprocessed
+#awk '{print $1,$5,$6,$11,$13}' $FILENAME > ${FILENAME}.preprocessed
 
 #filter // for /
 sed -i .bak 's/\/\//\//g' ${FILENAME}.preprocessed
@@ -24,3 +24,5 @@ sed -i .bak 's/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/#/g' ${FIL
 #filter uuid
 sed -i .bak 's/[a-fA-F0-9]\{8\}-[a-fA-F0-9]\{4\}-4[a-fA-F0-9]\{3\}-[89aAbB][a-fA-F0-9]\{3\}-[a-fA-F0-9]\{12\}/UUID/g' ${FILENAME}.preprocessed
 
+#removes line that starts with [
+sed -f .bak sed '/^\[/d' ${FILENAME}.preprocessed
