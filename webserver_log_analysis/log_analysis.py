5da7f6bedbb27223aa3a50562af844a3018a136b
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-09-04 14:04:25 -0300

    request time threshold to color messages, and setting some dtype

--- webserver_log_analysis/log_analysis.py
+++ webserver_log_analysis/log_analysis.py
@@ -4,6 +4,7 @@ import os
 from pandas import Series, DataFrame
 import pandas as pd
 from prettytable import PrettyTable
+from colors import Colors
 import numpy as np
 import sys
 import time
@@ -20,6 +21,7 @@ class LogAnalysis(object):
     def __init__(self, options={}):
 
         self.access_log = options.get("access_log")
+        self.request_time_threshold = options.get("request_time_threshold", 10)
         self.table_columns = ['request_uri','mean','min','max','std']
         self.table = PrettyTable(self.table_columns)
 
@@ -30,32 +32,24 @@ class LogAnalysis(object):
 
         if os.path.exists(self.access_log):
             columns = ['time_local','request_method','request_uri','status','request_time',]
-            df = pd.read_table(self.access_log, names=columns, sep='\s+')
+            #df = pd.read_table(self.access_log, names=columns, sep='\s+', dtype={"status": np.int8, "request_time": np.float16})
+            df = pd.read_table(self.access_log, names=columns, sep='\s+',)
             df_200 = df[df['status'] == 200]
             grouped = df_200.groupby('request_uri')
-            # interested_in = ['/network/create/',
-            #                  '/vip/remove/',
-            #                  '/api/pool/save/']
             for name, group in grouped:
                 desc = group.describe()
                 mean = desc['request_time']['mean']
                 min = desc['request_time']['min']
                 max = desc['request_time']['max']
                 std = desc['request_time']['std']
-                self.table.add_row([name,
+                c_init = Colors.OK
+                if mean >= self.request_time_threshold:
+                    c_init = Colors.FAIL
+                self.table.add_row([c_init + name,
                                     self.__round(mean),
                                     self.__round(min),
                                     self.__round(max),
-                                    self.__round(std)])
-            # for name in interested_in:
-            #     try:
-            #         group = grouped.get_group(name)
-            #         print name
-            #         print group.describe()
-            #         print "*" * 50
-            #     except Exception, e:
-            #         #LOG.warning("OPS... {0}".format(e))
-            #         pass
+                                    "{0}{1}".format(self.__round(std), Colors.END)])
         else:
             print LOG.debug("file [{0}] does not exist".format(self.access_log))
 
@@ -66,13 +60,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Nginx profiler')
     parser.add_argument('--log', type=str, default='',
                         help='Logfile')
+    parser.add_argument('--request_time_threshold', type=int, default=10,
+                        help='Threshold to color the row red')
 
     args = parser.parse_args()
 
     parser = SafeConfigParser()
 
     access_log = args.log
+    request_time_threshold = args.request_time_threshold
 
-    options = {"access_log": access_log,}
-    nginx = LogAnalysis(options=options)
-    nginx.run()
+    options = {"access_log": access_log,
+                "request_time_threshold": request_time_threshold}
+
+    log_analysis = LogAnalysis(options=options)
+    log_analysis.run()
