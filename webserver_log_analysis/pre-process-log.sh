39e1e5146e09a136460cc1f01e5cb7fb05c3e21b
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-09-04 11:27:49 -0300

    fix ip filter and uuid filter

--- webserver_log_analysis/pre-process-log.sh
+++ webserver_log_analysis/pre-process-log.sh
@@ -19,7 +19,8 @@ sed -i .bak 's/\/\//\//g' ${FILENAME}.preprocessed
 sed -i .bak "s/\/[0-9]\{0,9\}\//\/#\//g" ${FILENAME}.preprocessed
 
 #filter ip
-sed -i .bak 's/[0-9]\{3\}\.[0-9]\{3\}\.[0-9]\{3\}\.[0-9]\{3\}/#/g' ${FILENAME}.preprocessed
+sed -i .bak 's/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/#/g' ${FILENAME}.preprocessed
 
-#filter vm-uuid
-#sed -i .bak 's/vm\-.*[^\/]/vm-UUID/g' ${FILENAME}.preprocessed
+#filter uuid
+sed -i .bak 's/[a-fA-F0-9]\{8\}-[a-fA-F0-9]\{4\}-4[a-fA-F0-9]\{3\}-[89aAbB][a-fA-F0-9]\{3\}-[a-fA-F0-9]\{12\}/UUID/g' ${FILENAME}.preprocessed
+
