3cdd7b9e8d844b89b6d93103af81573f56d68ea6
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-09-04 14:19:46 -0300

    typo

--- webserver_log_analysis/pre-process-log.sh
+++ webserver_log_analysis/pre-process-log.sh
@@ -25,4 +25,4 @@ sed -i .bak 's/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/#/g' ${FIL
 sed -i .bak 's/[a-fA-F0-9]\{8\}-[a-fA-F0-9]\{4\}-4[a-fA-F0-9]\{3\}-[89aAbB][a-fA-F0-9]\{3\}-[a-fA-F0-9]\{12\}/UUID/g' ${FILENAME}.preprocessed
 
 #removes line that starts with [
-sed -f .bak sed '/^\[/d' ${FILENAME}.preprocessed
+sed -i .bak sed '/^\[/d' ${FILENAME}.preprocessed
