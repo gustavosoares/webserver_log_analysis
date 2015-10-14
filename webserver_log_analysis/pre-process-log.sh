54eaba8a31522114cd69e477dcfa099a4da03e67
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-10-14 18:02:54 -0300

    inverting filter rule

--- webserver_log_analysis/pre-process-log.sh
+++ webserver_log_analysis/pre-process-log.sh
@@ -15,11 +15,11 @@ awk '{print $7,$11,$12,$17,$19}' $FILENAME > ${FILENAME}.preprocessed
 #filter // for /
 sed -i .bak 's/\/\//\//g' ${FILENAME}.preprocessed
 
-#filter replace id for #
-sed -i .bak "s/\/[0-9]\{1,9\}/\/#/g" ${FILENAME}.preprocessed
-
 #filter ip
 sed -i .bak 's/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/#/g' ${FILENAME}.preprocessed
+
+#filter replace id for #
+sed -i .bak "s/\/[0-9]\{1,9\}/\/#/g" ${FILENAME}.preprocessed
 
 #filter uuid
 sed -i .bak 's/[a-fA-F0-9]\{8\}-[a-fA-F0-9]\{4\}-4[a-fA-F0-9]\{3\}-[89aAbB][a-fA-F0-9]\{3\}-[a-fA-F0-9]\{12\}/UUID/g' ${FILENAME}.preprocessed
