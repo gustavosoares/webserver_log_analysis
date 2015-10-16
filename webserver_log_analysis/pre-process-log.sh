e2150b35c3b7af20d0cb80354889ed6867000601
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-10-16 11:59:46 -0300

    bug fix id replace

--- webserver_log_analysis/pre-process-log.sh
+++ webserver_log_analysis/pre-process-log.sh
@@ -12,14 +12,20 @@ fi
 awk '{print $7,$11,$12,$17,$19}' $FILENAME > ${FILENAME}.preprocessed
 #awk '{print $1,$5,$6,$11,$13}' $FILENAME > ${FILENAME}.preprocessed
 
+# regexp online tester: http://www.regexr.com/
+
 #filter // for /
-sed -i .bak 's/\/\//\//g' ${FILENAME}.preprocessed
+sed -i .bak 's@//@/@g' ${FILENAME}.preprocessed
 
 #filter ip
 sed -i .bak 's/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/#/g' ${FILENAME}.preprocessed
 
 #filter replace id for #
-sed -i .bak "s/\/[0-9]\{1,9\}/\/#/g" ${FILENAME}.preprocessed
+#sed -i .bak "s/\/[0-9]\{1,9\}\//\/#\//g" ${FILENAME}.preprocessed
+sed -i .bak "s@/[0-9]\{1,9\}[/]@/#/@g" ${FILENAME}.preprocessed
+
+#filter replace uris ending with id without slash for #
+sed -i .bak "s@/[0-9]\{1,9\}[ ]@/#/ @g" ${FILENAME}.preprocessed
 
 #filter uuid
 sed -i .bak 's/[a-fA-F0-9]\{8\}-[a-fA-F0-9]\{4\}-4[a-fA-F0-9]\{3\}-[89aAbB][a-fA-F0-9]\{3\}-[a-fA-F0-9]\{12\}/UUID/g' ${FILENAME}.preprocessed
