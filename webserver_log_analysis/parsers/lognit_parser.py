69ba19affc63057bf98081029037a941e2d17c84
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-11-03 10:54:06 -0200

    uuid filter

--- webserver_log_analysis/parsers/lognit_parser.py
+++ webserver_log_analysis/parsers/lognit_parser.py
@@ -8,14 +8,10 @@ LOG = logging.getLogger(__name__)
 class LognitParser(BasicParser):
     """
 
-    #filter replace id for #
-    sed -i "s@/[0-9]\{1,9\}[/]@/#/@g" ${FILENAME}.preprocessed
 
-    #filter replace uris ending with id without slash for #
-    sed -i "s@/[0-9]\{1,9\}[ ]@/#/ @g" ${FILENAME}.preprocessed
 
     #filter uuid
-    sed -i 's/[a-fA-F0-9]\{8\}-[a-fA-F0-9]\{4\}-4[a-fA-F0-9]\{3\}-[89aAbB][a-fA-F0-9]\{3\}-[a-fA-F0-9]\{12\}/UUID/g' ${FILENAME}.preprocessed
+    sed -i 's//UUID/g' ${FILENAME}.preprocessed
 
     """
 
@@ -23,12 +19,12 @@ class LognitParser(BasicParser):
 
         self.columns_to_keep = [6,10,11,16,18] # 0 indexed
         self.reg = re.compile('.*HTTP/1.\d\" (?P<http_status_code>\d{3}) .*')
-        # patterns is list with tuples, containing the regexp and the replacement string
-        # \/[0-9]{1,9}[^.:]
+
         self.patterns = [("//","/"),
                          ("[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", "#"),
                          ("/[0-9]{1,9}/", "/#/"),
-                         ("/[0-9]{1,9} ", "/# "),]
+                         ("/[0-9]{1,9} ", "/# "),
+                         ("[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89aAbB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}", "UUID")]
 
 
     def parse_line(self, line):
